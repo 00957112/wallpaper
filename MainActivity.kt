0d40548bae8da4a79d6f36ab5ba5a08bf0a10866
00957112 <94968571+00957112@users.noreply.github.com>
2023-04-24 16:48:28 +0800

    Add files via upload

---
+++ MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.test
+
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import androidx.activity.result.contract.ActivityResultContracts
+
+class MainActivity : AppCompatActivity() {
+    lateinit var button:Button
+    lateinit var imgview:ImageView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        imgview=findViewById(R.id.img_view)
+
+        val imagePicker = registerForActivityResult(ActivityResultContracts.GetContent()) { uri: Uri? ->
+            uri?.let {
+                imgview.setImageURI(uri)
+            }
+        }
+
+        button=findViewById(R.id.btn)
+        button.setOnClickListener {
+            imagePicker.launch("image/*")
+        }
+
+
+}
+}
